96728c84be8c6fc8d820f7617805b6dfdd7b1d6d
juliette reverdy <juliettereverdy@MBP-de-juliette.concorde-pereire.loc>
2021-03-01 09:16:22 +0100

    debut page list

--- Screens/ListScreen.js
+++ Screens/ListScreen.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Card, ListItem, Button, Icon, Badge } from 'react-native-elements'
 
 
 export default function List() {
   return (
     <View style={styles.container}>
+      <ScrollView>
         <Text>List</Text>
+        <Card >
+          <Card.Image source={require('../assets/picture-2.jpg')}></Card.Image>
+                                 
+        </Card> 
+      </ScrollView>
     </View>
   );
 }
