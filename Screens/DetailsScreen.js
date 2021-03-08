0a41bb9aad79e3a52cfcdd42d6238dc95952d451
juliette reverdy <reverdyjuliette@gmail.com>
2021-03-08 13:11:06 +0100

    DetailsScreen terminée

--- Screens/DetailsScreen.js
+++ Screens/DetailsScreen.js
@@ -16,24 +16,18 @@ function Details(props) {
 
   const [genderChosen, setGenderChosen] = useState(null);
   const [genderVisible, setGenderVisible] = useState(false);
-  //const [genderText, setGenderText] = useState('Choisir');
+  
 
   const [lengthVisible, setLengthVisible] = useState(false);
-  //const [lengthText, setLengthText] = useState('Choisir');
   const [length, setLength] = useState(null);
 
   const [typeVisible, setTypeVisible] = useState(false);
-  //const [typeText, setTypeText] = useState('Choisir');
   const [type, setType] = useState(null);
   const [validationVisible, setValidationVisible] = useState(false);
 
     
   useEffect(() => {
-    // if (props.communication.user.gender) {
-    //   setGenderText(props.communication.user.gender);
-    // } else {
-    //   setGenderText('Choisir')
-    // }
+
     setGenderChosen(props.communication.user.gender);
     setLength(props.communication.user.hairLength);
     setType(props.communication.user.hairType);
@@ -53,19 +47,15 @@ function Details(props) {
   const chosenGender = (element) => {
     setGenderChosen(element);
     setGenderVisible(false);
-    //setGenderText(element);
     setLength(null);
     setType(null);
-    //setLengthText('Choisir');
   }
 
   var closeGender = () => {
     setGenderVisible(false);
     setGenderChosen(null);
-    //setGenderText('Choisir');
     setLength(null);
     setType(null)
-    //setLengthText('Choisir');
   }
 
   //LENGTH
@@ -73,53 +63,33 @@ function Details(props) {
   if (genderChosen === 'une femme') {
       if (length === 'très court') {
         imageLength = 
-          //<Pressable style={styles.image} onPress={() => openLength()}>
             <Image style={{width: 70, height: 90}}source={require('../assets/Wlength1.png')} />
-          //</Pressable>
       } else if (length === 'court') {
         imageLength = 
-        //<Pressable style={styles.image} onPress={() => openLength()}>
-          <Image style={{width: 70, height: 90}}source={require('../assets/Wlength2.png')} />
-        //</Pressable>
-         
+          <Image style={{width: 70, height: 90}}source={require('../assets/Wlength2.png')} /> 
       } else if (length === 'mi-long') {
         imageLength = 
-        // <Pressable style={styles.image} onPress={() => openLength()}>
           <Image style={{width: 70, height: 90}}source={require('../assets/Wlength3.png')} />
-        // </Pressable>
       } else if (length === 'long') {
         imageLength = 
-        //<Pressable style={styles.image} onPress={() => openLength()}>
-          <Image style={{width: 70, height: 90}}source={require('../assets/Wlength4.png')} />
-        //</Pressable>        
+          <Image style={{width: 70, height: 90}}source={require('../assets/Wlength4.png')} />      
       } else if (length === 'très long') {
         imageLength = 
-        //<Pressable style={styles.image} onPress={() => openLength()}>
           <Image style={{width: 70, height: 90}}source={require('../assets/Wlength5.png')} />
-        //</Pressable> 
       }
   } else {
     if (length === 'très court') {
       imageLength = 
-        //<Pressable style={styles.image} onPress={() => openLength()}>
           <Image style={{width: 50, height: 70}}source={require('../assets/Mlength1.png')} />
-        //</Pressable>
     } else if (length === 'court') {
       imageLength = 
-      //<Pressable style={styles.image} onPress={() => openLength()}>
-        <Image style={{width: 50, height: 70}}source={require('../assets/Mlength2.png')} />
-      //</Pressable>
-       
+        <Image style={{width: 50, height: 70}}source={require('../assets/Mlength2.png')} />     
     } else if (length === 'mi-long') {
       imageLength = 
-      //<Pressable style={styles.image} onPress={() => openLength()}>
         <Image style={{width: 50, height: 70}}source={require('../assets/Mlength3.png')} />
-      //</Pressable>
     } else if (length === 'long') {
       imageLength = 
-      //<Pressable style={styles.image} onPress={() => openLength()}>
-        <Image style={{width: 50, height: 70}}source={require('../assets/Mlength4.png')} />
-      //</Pressable>        
+        <Image style={{width: 50, height: 70}}source={require('../assets/Mlength4.png')} />      
     } 
   }
 
@@ -133,13 +103,11 @@ function Details(props) {
 
   var chosenLength = (longueur) => {
     setLength(longueur);
-    //setLengthText(longueur);
     setLengthVisible(false);
   }
   
   var closeLength = () => {
     setLengthVisible(false);
-    //setLengthText('Choisir');
     setLength(null)
   };
 
@@ -154,13 +122,11 @@ function Details(props) {
 
   var closeType = () => {
     setTypeVisible(false);
-    //setTypeText('Choisir');
     setType(null);
   }
 
   var chosenType = (type) => {
     setType(type);
-    //setTypeText(type);
     setTypeVisible(false);
   }
 
@@ -170,53 +136,34 @@ function Details(props) {
   if (genderChosen === 'une femme') {
       if (type=== 'raide') {
         imageType = 
-          // <Pressable style={styles.image} onPress={() => openType()}>
             <Image style={{width: 70, height: 90}}source={require('../assets/Wtype1.png')} />
-          // </Pressable>
       } else if (type === 'ondulé') {
         imageType = 
-        //<Pressable style={styles.image} onPress={() => openType()}>
-          <Image style={{width: 70, height: 90}}source={require('../assets/Wtype2.png')} />
-        //</Pressable>
-         
+          <Image style={{width: 70, height: 90}}source={require('../assets/Wtype2.png')} />     
       } else if (type === 'bouclé') {
         imageType = 
-        //<Pressable style={styles.image} onPress={() => openType()}>
           <Image style={{width: 70, height: 90}}source={require('../assets/Wtype3.png')} />
-        //</Pressable>
       } else if (type === 'frisé') {
         imageType = 
-        //<Pressable style={styles.image} onPress={() => openType()}>
-          <Image style={{width: 70, height: 90}}source={require('../assets/Wtype4.png')} />
-        //</Pressable>        
+          <Image style={{width: 70, height: 90}}source={require('../assets/Wtype4.png')} />     
       } else if (type === 'crépu') {
         imageType = 
-        //<Pressable style={styles.image} onPress={() => openType()}>
           <Image style={{width: 70, height: 90}}source={require('../assets/Wtype5.png')} />
-        //</Pressable> 
       }
   } else {
     if (type === 'raide') {
       imageType = 
-        //<Pressable style={styles.image} onPress={() => openType()}>
+
           <Image style={{width: 50, height: 70}}source={require('../assets/Mtype1.png')} />
-        //</Pressable>
     } else if (type === 'ondulé') {
       imageType = 
-      //<Pressable style={styles.image} onPress={() => openType()}>
         <Image style={{width: 50, height: 70}}source={require('../assets/Mtype2.png')} />
-      //</Pressable>
-       
     } else if (type === 'bouclé') {
       imageType = 
-      //<Pressable style={styles.image} onPress={() => openType()}>
         <Image style={{width: 50, height: 70}}source={require('../assets/Mtype3.png')} />
-      //</Pressable>
     } else if (type === 'crépu') {
       imageType = 
-      //<Pressable style={styles.image} onPress={() => openType()}>
-        <Image style={{width: 50, height: 70}}source={require('../assets/Mtype4.png')} />
-      //</Pressable>        
+        <Image style={{width: 50, height: 70}}source={require('../assets/Mtype4.png')} />   
     } 
   }
 
@@ -280,8 +227,9 @@ function Details(props) {
 
   return (
     <View style={globalStyles.container}>
+
       <View>
-      <Pressable onPress={() => setGenderVisible(true)}>
+          <Pressable onPress={() => setGenderVisible(true)}>
             <Text style={styles.title}>JE SUIS: </Text>
             {genderChosen != null ?
             <Text style={ {textAlign: 'center', fontSize: 20}}>{genderChosen}</Text>
@@ -297,7 +245,7 @@ function Details(props) {
             <Pressable onPress={() => openLength()}>
               <Text style={styles.title}>J'AI LES CHEVEUX: </Text>
               {length != null ? 
-              <View style={{display: 'flex', alignItems: 'center'}}>{imageLength}</View>
+              <View style={{display: 'flex', alignItems: 'center'}}>{imageLength}<Text>{length}</Text></View>
               : 
               <Text>Choisir</Text>
               }
@@ -309,7 +257,7 @@ function Details(props) {
             <Pressable onPress={() => openType()}>
               <Text style={styles.title}>ET: </Text>
               {type != null ? 
-              <View style={{display: 'flex', alignItems: 'center'}}>{imageType}</View>
+              <View style={{display: 'flex', alignItems: 'center'}}>{imageType}<Text>{type}</Text></View>
               : 
               <Text>Choisir</Text>
               }
@@ -317,30 +265,10 @@ function Details(props) {
             </Pressable>
       </View>
 
-      {/* <View style={{display: 'flex', alignItems: 'center', marginTop: 30}}>
-        <Text style={styles.titleText}>Je suis:</Text>
-        <Button title={genderText} color='white' backgroundColor='#4280AB' onPress={() => setGenderVisible(true)}></Button>
-      </View>
-      <Text style={{textAlign: 'center', paddingTop: 5}}>{errorText}</Text> */}
-
-      {/* <View style={{display: 'flex', alignItems: 'center', marginTop: 30}}>
-        <Text style={styles.titleText}>J'ai les cheveux:</Text>
-        {length != null ? 
-        imageLength
-        : <Button title='Choisir' onPress={() => openLength()} color='white' backgroundColor='#4280AB'/>
-        }
-      </View> */}
-
-      {/* <View style={{display: 'flex', alignItems: 'center', marginTop: 30}}>
-        <Text style={styles.titleText}>Et:</Text>
-        {type != null ?
-        imageType
-      : <Button title='Choisir' onPress={() => openType()} color='white' backgroundColor='#4280AB'/>
-      }
-      </View> */}
       <View style={{margin: 20, alignItems: 'center'}}>
         <Button  title='Envoyer à mon coiffeur' backgroundColor='#4280AB' color='white' onPress={() => validation()}></Button>
       </View>
+
       <View style={{margin: 20, alignItems: 'center'}}>
         <Button width={150} title='Retour' backgroundColor='#AB4242' color='white' onPress={() => props.navigation.navigate('Profile')}></Button>
       </View>
@@ -361,33 +289,42 @@ function Details(props) {
             <View style={{display: 'flex', flexDirection: 'row'}}>
               <Pressable style={styles.image} onPress={() => chosenLength('très court')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Wlength1.png')} />
+                <Text style={styles.miniText}>très court</Text>
               </Pressable>
               <Pressable style={styles.image} onPress={() => chosenLength('court')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Wlength2.png')}/>
+                <Text style={styles.miniText}>court</Text>
               </Pressable>
               <Pressable style={styles.image} onPress={() => chosenLength('mi-long')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Wlength3.png')}/>
+                <Text style={styles.miniText}>mi-long</Text>
               </Pressable>
               <Pressable style={styles.image} onPress={() => chosenLength('long')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Wlength4.png')}/>
+                <Text style={styles.miniText}>long</Text>
               </Pressable>
               <Pressable style={styles.image} onPress={() => chosenLength('très long')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Wlength5.png')}/>
+                <Text style={styles.miniText}>très long</Text>
               </Pressable>
             </View>
             : 
             <View style={{display: 'flex', flexDirection: 'row'}}>
               <Pressable style={styles.image} onPress={() => chosenLength('très court')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Mlength1.png')} />
+                <Text style={styles.miniText}>très court</Text>
               </Pressable>
               <Pressable style={styles.image} onPress={() => chosenLength('court')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Mlength2.png')}/>
+                <Text style={styles.miniText}>court</Text>
               </Pressable>
               <Pressable style={styles.image} onPress={() => chosenLength('mi-long')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Mlength3.png')}/>
+                <Text style={styles.miniText}>mi-long</Text>
               </Pressable>
               <Pressable style={styles.image} onPress={() => chosenLength('long')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Mlength4.png')}/>
+                <Text style={styles.miniText}>long</Text>
               </Pressable>
             </View>
             }
@@ -401,33 +338,42 @@ function Details(props) {
             <View style={{display: 'flex', flexDirection: 'row'}}>
               <Pressable style={styles.image} onPress={() => chosenType('raide')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Wtype1.png')} />
+                <Text style={styles.miniText}>raide</Text>
               </Pressable>
               <Pressable style={styles.image} onPress={() => chosenType('ondulé')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Wtype2.png')}/>
+                <Text style={styles.miniText}>ondulé</Text>
               </Pressable>
               <Pressable style={styles.image} onPress={() => chosenType('bouclé')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Wtype3.png')}/>
+                <Text style={styles.miniText}>bouclé</Text>
               </Pressable>
               <Pressable style={styles.image} onPress={() => chosenType('frisé')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Wtype4.png')}/>
+                <Text style={styles.miniText}>frisé</Text>
               </Pressable>
               <Pressable style={styles.image} onPress={() => chosenType('crépu')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Wtype5.png')}/>
+                <Text style={styles.miniText}>crépu</Text>
               </Pressable>
             </View>
             : 
             <View style={{display: 'flex', flexDirection: 'row'}}>
               <Pressable style={styles.image} onPress={() => chosenType('raide')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Mtype1.png')} />
+                <Text style={styles.miniText}>raide</Text>
               </Pressable>
               <Pressable style={styles.image} onPress={() => chosenType('ondulé')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Mtype2.png')}/>
+                <Text style={styles.miniText}>ondulé</Text>
               </Pressable>
               <Pressable style={styles.image} onPress={() => chosenType('bouclé')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Mtype3.png')}/>
+                <Text style={styles.miniText}>bouclé</Text>
               </Pressable>
               <Pressable style={styles.image} onPress={() => chosenType('crépu')}>
                 <Image style={{width: 50, height: 70}}source={require('../assets/Mtype4.png')}/>
+                <Text style={styles.miniText}>crépu</Text>
               </Pressable>
             </View>
             }
@@ -470,6 +416,7 @@ const styles = StyleSheet.create({
   },
   image: {padding: 5},
   title: {fontWeight : "bold", fontSize: 20, marginTop:10,},
+  miniText: {fontSize: 10, textAlign: 'center'}
 });
 
 const mapStateToProps = (state) => {
