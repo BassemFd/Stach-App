cb4795779cb5bd3b5f6698342399e507e329f3d8
Bassem <bassem.fayed@gmail.com>
2021-03-16 11:52:42 +0100

    removed api key

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -33,7 +33,7 @@ function HomeScreen(props) {
   const [priceFork, setPriceFork] = useState(null);
   const [rating, setRating] = useState(null) 
 
-  const GOOGLE_PLACES_API_KEY = 'AIzaSyDhW13-YcWkEnPgvmEfBPu_IOJ2go6Evws';
+  // const GOOGLE_PLACES_API_KEY = 'Insert API key';
   const ref = useRef();
   
   // Adjusting slider to window screen
